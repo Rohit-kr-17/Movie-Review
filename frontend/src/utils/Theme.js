b60c8f978c5531ee1280cf23d5b7f7d8201c4ea2
Rohit-kr-17 <rohit.2001.chaurasia@gmail.com>
2023-07-23 15:55:32 +0530

    Live Search field

--- frontend/src/utils/Theme.js
+++ frontend/src/utils/Theme.js
@@ -1,2 +1,5 @@
 export const commonMdalClasses =
 	"dark:bg-secondary bg-white drop-shadow-lg rounded p-6 space-y-6";
+
+export const commonInputClasses =
+	"w-full outline-none dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary transition bg-transparent dark:text-white text-primary";
